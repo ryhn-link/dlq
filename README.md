298246807eb3ff5f6fdf16ef4de24edde6ec8abb
Ryhon <ryhon0@gmail.com>
2021-03-06 21:42:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# dlq (D language query)
+dlq is a `System.Linq` port to D.
+Based on [corefx source code](https://github.com/dotnet/corefx/tree/master/src/System.Linq/src/System/Linq) and [official Microsoft documentation](https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable?view=net-5.0)
+
+## Ported functions
+- [ ] Aggregate  
+- [x] All  
+- [x] Any  
+- [x] Average  
+- [ ] Cast  
+- [ ] DefaultIfEmpty   
+- [x] Distinct   
+- [ ] Except
+- [x] First
+- [ ] FirstOrDefault
+- [ ] GroupBy
+- [ ] GroupJoin
+- [ ] Intersect
+- [ ] Join
+- [x] Last
+- [ ] LastOrDefault
+- [x] LongCount
+- [x] Max
+- [x] Min
+- [ ] OfType
+- [x] OrderBy
+- [x] OrderByDescending
+- [ ] Prepend
+- [x] Select (HACK)
+- [ ] Skip
+- [ ] SkipLast
+- [ ] SkipWhile
+- [x] Sum
+- [ ] Take
+- [ ] TakeLast
+- [ ] TakeWhile
+- [ ] ToDictionary
+- [ ] Union
+- [x] Where
+- [ ] Zip
+
+### Won't add
+- Append - D already lets you append arrays using `~=`
+- AsEnumerable - This project only uses arrays
+- Contains - D lets you do that using the `x in y`
+- ElementAt - `[n]` gets the element at n-th position
+- Empty - `[ ]` creates an empty array
+- Reverse - `array` already has a property with the same name that does the same thing 
+- SelectMany - Just run Select multiple times
+- SequenceEqual - `[] == []` does the same thing
+- Single - What's the point? Just check the length and use `first`
+- ThenBy - D doesn't have deferred execution
+- ThenByDescending - same as `ThenBy`
+- ToArray - Same as `AsEnumerable`
+- ToHashSet - Same as `AsEnumerable`
+- ToList - Same as `AsEnumerable`
+- ToLookup - Same as `AsEnumerable`
---
+++ source/app.d
@@ -0,0 +1,191 @@
+module app;
+
+void main()
+{
+	import std.stdio;
+	import std.string;
+
+	writeln(["a", "b", "c"].first!(s => s == "b"));
+
+	writeln(["a", "b", "c"].where!(s => s != "a"));
+
+	writeln(["A", "B", "C"].select!(s => s.toLower, string));
+
+	writeln([6, 77, 2, 86, 15, 65].orderBy!(a => a));
+}
+
+import std.exception;
+
+/// Returns the first element in an array that meets the conditionition
+T first(alias condition = x => true, T)(T[] source)
+{
+	enforce(source.length > 0, "Array length cannot be 0");
+	foreach (k; source)
+	{
+		if (condition(k))
+			return k;
+	}
+	return null;
+}
+
+/// Returns the first element in an array that meets the conditionition
+T firstOrDefault(alias condition = x => true, T)(T[] source)
+{
+	enforce(source.length > 0, "Array length cannot be 0");
+	foreach (k; source)
+	{
+		if (condition(k))
+			return k;
+	}
+	return null;
+}
+
+/// Returns the last element in an array that meets the condition
+T last(alias condition = x => true, T)(T[] source)
+{
+	for (int i = source.length; i >= 0; i--)
+	{
+		T v = source[i];
+		if (condition(v))
+			return v;
+	}
+}
+
+/// Returns an array of elements that meet the condition 
+T[] where(alias condition = x => true, T)(T[] source)
+{
+	T[] results;
+	foreach (k; source)
+	{
+		if (condition(k))
+			results ~= k;
+	}
+	return results;
+}
+
+/// Checks if array contains one or more item that meets the condition
+bool any(alias condition = x => true, T)(T[] source)
+{
+	foreach (k; source)
+	{
+		if (condition(k))
+			return true;
+	}
+	return false;
+}
+
+/// Counts how many items meet the conditions
+int count(alias condition = x => true, T)(T[] source)
+{
+	int i = 0;
+	foreach (k; source)
+	{
+		if (condition(k))
+			i++;
+	}
+	return i;
+}
+
+/// Counts how many items meet the conditions
+long longCount(alias condition = x => true, T)(T[] source)
+{
+	long i = 0;
+	foreach (k; source)
+	{
+		if (condition(k))
+			i++;
+	}
+	return i;
+}
+
+// HACK
+// TODO
+// Get rid of TResult and figure out the type from the delegate,
+// probably use something like ReturnType!selector[] but it doesn't work
+//
+// Currently:
+//   ["a","b","c"].select!(x => x.toLower, string)
+//
+// Goal:
+//   ["a","b","c"].select!(x => x.toLower)
+
+/// Selects specified members from objects
+TResult[] select(alias selector, TResult, TSource)(TSource[] source)
+{
+	TResult[] results;
+	foreach (k; source)
+	{
+		results ~= selector(k);
+	}
+	return results;
+}
+
+/// Returns an array of distinct items
+T[] distinct(T)(T[] source)
+{
+	T[] results;
+	foreach (k; source)
+	{
+		if (!k in results)
+			results ~= k;
+	}
+	return results;
+}
+
+/// Returns the largest item in the array
+T max(T)(T[] source)
+{
+	T max;
+	foreach (k; source)
+	{
+		if (k > max)
+			max = k;
+	}
+	return max;
+}
+
+/// Returns the smallest item in the array
+T min(T)(T[] source)
+{
+	T min;
+	foreach (k; source)
+	{
+		if (k < min)
+			min = k;
+	}
+	return min;
+}
+
+/// Returns the average value of all items in the array
+T average(T)(T[] source)
+{
+	return source.sum / source.length;
+}
+
+/// Retuns the sum of all values in the array
+T sum(T)(T[] source)
+{
+	foreach (k; source)
+	{
+		sum ~= k;
+	}
+	return sum / source.length;
+}
+
+/// Sorts the elements of a sequence in ascending order according to a key
+T[] orderBy(alias func, T)(T[] source)
+{
+	import std.algorithm.sorting;
+
+	alias compare = (x, y) => func(x) > func(y);
+	return source.sort!(compare).release;
+}
+
+/// Sorts the elements of a sequence in descending order according to a key
+T[] orderByDescending(alias func = a => a, T)(T[] source)
+{
+	import std.algorithm.sorting;
+
+	alias compare = (x, y) => func(x) < func(y);
+	return source.sort!(compare).release;
+}
